1766bdd03ced276dafd00015cdaf9ddee6641b6a
wasim2263 <abdullahwasim42@gmail.com>
2023-03-08 11:35:42 +0200

    Changed adress field to address in Client model

--- app/Client.php
+++ app/Client.php
@@ -10,7 +10,7 @@ class Client extends Model
         'name',
         'email',
         'phone',
-        'adress',
+        'address',
         'city',
         'postcode',
     ];
--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -36,7 +36,7 @@ class ClientsController extends Controller
         $client->name = $request->get('name');
         $client->email = $request->get('email');
         $client->phone = $request->get('phone');
-        $client->adress = $request->get('adress');
+        $client->address = $request->get('address');
         $client->city = $request->get('city');
         $client->postcode = $request->get('postcode');
         $client->save();
