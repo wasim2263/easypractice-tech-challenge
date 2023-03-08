805e48cc340c061f4a9928497b66c6b23dfb10f9
wasim2263 <abdullahwasim42@gmail.com>
2023-03-08 14:46:51 +0200

    Fixed full day name and full month format

--- app/Booking.php
+++ app/Booking.php
@@ -25,13 +25,13 @@ class Booking extends Model
     {
 //        can be improved using different approach. But this one is simple and much readable
         if ($this->start->format("y") != $this->end->format("y")) {
-            $formattedBookingTime = "{$this->start->format("D d M Y, h:m")} to {$this->end->format("D d M Y, h:m")}";
+            $formattedBookingTime = "{$this->start->format("l d F Y, h:m")} to {$this->end->format("l d F Y, h:m")}";
         } elseif ($this->start->format("m") != $this->end->format("m")) {
-            $formattedBookingTime = "{$this->start->format("D d M Y, h:m")} to {$this->end->format("D d M, h:m")}";
+            $formattedBookingTime = "{$this->start->format("l d F Y, h:m")} to {$this->end->format("l d F, h:m")}";
         }elseif ($this->start->format("d") != $this->end->format("d")){
-            $formattedBookingTime = "{$this->start->format("D d M Y, h:m")} to {$this->end->format("D d, h:m")}";
+            $formattedBookingTime = "{$this->start->format("l d F Y, h:m")} to {$this->end->format("l d, h:m")}";
         }else{
-            $formattedBookingTime = "{$this->start->format("D d M Y, h:m")} to {$this->end->format("h:m")}";
+            $formattedBookingTime = "{$this->start->format("l d F Y, h:m")} to {$this->end->format("h:m")}";
         }
         return $formattedBookingTime;
     }
