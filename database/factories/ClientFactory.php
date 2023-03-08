77b0ddd1e0fe2f6a442bf139d426305126a23075
wasim2263 <abdullahwasim42@gmail.com>
2023-03-08 12:40:24 +0200

    Fixed db seeder problem in client factory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -10,7 +10,7 @@ $factory->define(Client::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->email,
         'phone' => $faker->phoneNumber,
-        'adress' => $faker->streetAddress,
+        'address' => $faker->streetAddress,
         'city' => $faker->city,
         'postcode' => $faker->postcode,
     ];
